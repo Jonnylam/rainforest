07d7931cc8af97538daac6626543199ff816df43
jonnylam <jonathanryanlam@gmail.com>
2015-02-24 16:22:31 -0500

    added search function onto product.js file

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,1 +1,14 @@
-products.js
+$(document).ready(function(){
+  $('#search-form').submit(function(event) {
+    event.preventDefault();
+    var searchValue = $('#search').val();
+
+    $.ajax({
+      url: '/products?search=' + searchValue,
+      type: 'GET',
+      dataType: 'html'
+    }).done(function(data){
+      $('#products').html(data);
+    });
+  });
+});
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,11 @@ class ProductsController < ApplicationController
     else
       Product.all
     end
+
+    if request.xhr?
+      render @products
+    end
+
   end
 
   def show
