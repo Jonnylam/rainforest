2269c31acd229afd952f5cf43cf75a9b2ad17062
jonnylam <jonathanryanlam@gmail.com>
2015-02-24 16:54:04 -0500

    added reviews to Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,11 +15,20 @@
 	print "|"
 end
 
-# 100.times do
-# 	User.create!(
-# 		email: Faker::Internet.email,
-# 		name:  Faker::Name.name,
-# 		password: "asdf;lkj"
-# 	)
-# 	print "|"
-# end
+100.times do
+	User.create!(
+		email: Faker::Internet.email,
+		name:  Faker::Name.name,
+		password: "asdf;lkj"
+	)
+	print "|"
+end
+
+1000.times do 
+	Review.create!(
+	product_id: rand(100),
+	user_id: rand(100),
+	comment: Faker::Lorem.sentence(3)
+	)
+	print "|"
+end
